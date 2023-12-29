955be96cd2684ffbef139b82dbf48deb9733fe70
Sisby folk <55819817+sisby-folk@users.noreply.github.com>
2023-12-29 15:13:59 +1100

    Update make_profile.bat

--- make_profile.bat
+++ make_profile.bat
@@ -4,7 +4,7 @@ setlocal
 set SOURCE_FOLDER=profile
 set ZIP_FILE=profile.zip
 
-powershell Compress-Archive -Force profile/* profile.zip
+powershell Compress-Archive -Force profile/* "Modfest 1.20 - Sky and Sea.zip"
 
 echo Done
 endlocal
